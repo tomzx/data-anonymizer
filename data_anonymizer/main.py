33a123e1084d6982b297134ee495b101809bf571
Tom Rochette <roctom@gmail.com>
2019-12-19 18:07:43 -0500

    Fix entry point script

--- data_anonymizer/main.py
+++ data_anonymizer/main.py
@@ -4,138 +4,152 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, OrdinalEncoder, Binarizer
 
-argument_parser = ArgumentParser()
-argument_parser.add_argument("file", help="Path to data file")
-argument_parser.add_argument("output", help="Path where to store the anonymized data")
-argument_parser.add_argument(
-    "--feature-anonymize", nargs="+", help="Remove feature name (* to anonymze all)"
-)
-argument_parser.add_argument(
-    "--feature-binarize", nargs="+", help="Feature to binarize (either 0 or 1)"
-)
-argument_parser.add_argument(
-    "--feature-clamp",
-    nargs="+",
-    help="Feature is limited between min/max (last 2 values)",
-)
-argument_parser.add_argument(
-    "--feature-categorize", nargs="+", help="Categorical feature to anonymize"
-)
-argument_parser.add_argument(
-    "--feature-fill",
-    nargs="+",
-    help="Feature is replaced by fill value (last provided value)",
-)
-argument_parser.add_argument(
-    "--feature-min-max-scale", nargs="+", help="Feature to scale between 0 and 1"
-)
-argument_parser.add_argument("--feature-remove", nargs="+", help="Feature to remove")
-argument_parser.add_argument(
-    "--feature-round",
-    nargs="+",
-    help="Round feature to closest multiple (last provided value)",
-)
 
-args = argument_parser.parse_args()
-
-df = pd.read_csv(args.file, comment="#")
-
-transforms = []
-
-# Remove features
-if args.feature_remove:
-    df = df.drop(columns=args.feature_remove)
-
-# Scale features between 0 and 1
-min_max_scaler = MinMaxScaler()
-
-if args.feature_min_max_scale:
-    min_max_scaler.fit(df[args.feature_min_max_scale])
-
-    df[args.feature_min_max_scale] = min_max_scaler.transform(
-        df[args.feature_min_max_scale]
+def run():
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("file", help="Path to data file")
+    argument_parser.add_argument(
+        "output", help="Path where to store the anonymized data"
+    )
+    argument_parser.add_argument(
+        "--feature-anonymize", nargs="+", help="Remove feature name (* to anonymze all)"
+    )
+    argument_parser.add_argument(
+        "--feature-binarize", nargs="+", help="Feature to binarize (either 0 or 1)"
+    )
+    argument_parser.add_argument(
+        "--feature-clamp",
+        nargs="+",
+        default=[],
+        help="Feature is limited between min/max (last 2 values)",
+    )
+    argument_parser.add_argument(
+        "--feature-categorize", nargs="+", help="Categorical feature to anonymize"
+    )
+    argument_parser.add_argument(
+        "--feature-fill",
+        nargs="+",
+        default=[],
+        help="Feature is replaced by fill value (last provided value)",
+    )
+    argument_parser.add_argument(
+        "--feature-min-max-scale",
+        nargs="+",
+        default=[],
+        help="Feature to scale between 0 and 1",
+    )
+    argument_parser.add_argument(
+        "--feature-remove", nargs="+", help="Feature to remove"
+    )
+    argument_parser.add_argument(
+        "--feature-round",
+        nargs="+",
+        default=[],
+        help="Round feature to closest multiple (last provided value)",
     )
 
-    transforms += [f"feature-min-max-scale {' '.join(args.feature_min_max_scale)}"]
+    args = argument_parser.parse_args()
 
-# Binarize
-binarizer = Binarizer(threshold=0.5)
+    df = pd.read_csv(args.file, comment="#")
 
-if args.feature_binarize:
-    min_max_scaler.fit(df[args.feature_binarize])
+    transforms = []
 
-    df[args.feature_binarize] = min_max_scaler.transform(df[args.feature_binarize])
+    # Remove features
+    if args.feature_remove:
+        df = df.drop(columns=args.feature_remove)
 
-    df[args.feature_binarize] = binarizer.transform(df[args.feature_binarize])
+    # Scale features between 0 and 1
+    min_max_scaler = MinMaxScaler()
 
-    transforms += [f"feature-binarize {' '.join(args.feature_binarize)}"]
+    if args.feature_min_max_scale:
+        min_max_scaler.fit(df[args.feature_min_max_scale])
 
-# Categorize
-ordinal_encoder = OrdinalEncoder(dtype=np.int)
+        df[args.feature_min_max_scale] = min_max_scaler.transform(
+            df[args.feature_min_max_scale]
+        )
 
-if args.feature_categorize:
-    ordinal_encoder.fit(df[args.feature_categorize])
+        transforms += [f"feature-min-max-scale {' '.join(args.feature_min_max_scale)}"]
 
-    # Replace categorical columns by letters
-    df[args.feature_categorize] = ordinal_encoder.transform(df[args.feature_categorize])
-    # Prefix with C to indicate it's a categorical
-    df[args.feature_categorize] = "C" + df[args.feature_categorize].astype(str)
+    # Binarize
+    binarizer = Binarizer(threshold=0.5)
 
-    transforms += [f"feature-categorize {' '.join(args.feature_categorize)}"]
+    if args.feature_binarize:
+        min_max_scaler.fit(df[args.feature_binarize])
 
-# Fill
-# TODO: Support different fill per column
-if len(args.feature_fill) > 1:
-    df[args.feature_fill[:-1]] = args.feature_fill[-1]
+        df[args.feature_binarize] = min_max_scaler.transform(df[args.feature_binarize])
 
-    transforms += [f"feature-fill {' '.join(args.feature_fill)}"]
+        df[args.feature_binarize] = binarizer.transform(df[args.feature_binarize])
 
-# Clamp
-# TODO: Support different clamp min/max per column
-if len(args.feature_clamp) > 2:
-    features = args.feature_clamp[:-2]
-    minimum = float(args.feature_clamp[-2])
-    df[features] = df[features].where(df[features] >= minimum, other=minimum)
+        transforms += [f"feature-binarize {' '.join(args.feature_binarize)}"]
 
-    maximum = float(args.feature_clamp[-1])
-    df[features] = df[features].where(df[features] <= maximum, other=maximum)
+    # Categorize
+    ordinal_encoder = OrdinalEncoder(dtype=np.int)
 
-    transforms += [f"feature-clamp {' '.join(args.feature_clamp)}"]
+    if args.feature_categorize:
+        ordinal_encoder.fit(df[args.feature_categorize])
 
-# Round
-if len(args.feature_round) > 1:
-    multiple = float(args.feature_round[-1])
-    df[args.feature_round[:-1]] = (
-        round(df[args.feature_round[:-1]] / multiple) * multiple
-    )
+        # Replace categorical columns by letters
+        df[args.feature_categorize] = ordinal_encoder.transform(
+            df[args.feature_categorize]
+        )
+        # Prefix with C to indicate it's a categorical
+        df[args.feature_categorize] = "C" + df[args.feature_categorize].astype(str)
 
-    transforms += [f"feature-round {' '.join(args.feature_round)}"]
+        transforms += [f"feature-categorize {' '.join(args.feature_categorize)}"]
 
-# TODO: Use the robust scaler to ignore outliers
+    # Fill
+    # TODO: Support different fill per column
+    if len(args.feature_fill) > 1:
+        df[args.feature_fill[:-1]] = args.feature_fill[-1]
 
-# Anonymize list of features
-if args.feature_anonymize:
-    if "*" in args.feature_anonymize:
-        features_anonymize = df.columns
-    else:
-        features_anonymize = args.feature_anonymize
+        transforms += [f"feature-fill {' '.join(args.feature_fill)}"]
 
-    mapping = {feature: i for i, feature in enumerate(features_anonymize)}
+    # Clamp
+    # TODO: Support different clamp min/max per column
+    if len(args.feature_clamp) > 2:
+        features = args.feature_clamp[:-2]
+        minimum = float(args.feature_clamp[-2])
+        df[features] = df[features].where(df[features] >= minimum, other=minimum)
 
-    df = df.rename(columns=mapping)
+        maximum = float(args.feature_clamp[-1])
+        df[features] = df[features].where(df[features] <= maximum, other=maximum)
 
-    for i, transform in enumerate(transforms):
-        # Add a space at the end so we can match the last input
-        transform += " "
-        for key, value in mapping.items():
-            transform = transform.replace(f" {key} ", f" {value} ")
+        transforms += [f"feature-clamp {' '.join(args.feature_clamp)}"]
 
-        transforms[i] = transform
+    # Round
+    if len(args.feature_round) > 1:
+        multiple = float(args.feature_round[-1])
+        df[args.feature_round[:-1]] = (
+            round(df[args.feature_round[:-1]] / multiple) * multiple
+        )
 
-with open(args.output, "w") as f:
-    for transform in transforms:
-        f.write("# " + transform + "\n")
-    df.to_csv(f, index=False, line_terminator="\n")
+        transforms += [f"feature-round {' '.join(args.feature_round)}"]
 
-# TODO: Generate a mapping so that having the initial data allows you to understand
-# someone referring to the transformed data
+    # TODO: Use the robust scaler to ignore outliers
+
+    # Anonymize list of features
+    if args.feature_anonymize:
+        if "*" in args.feature_anonymize:
+            features_anonymize = df.columns
+        else:
+            features_anonymize = args.feature_anonymize
+
+        mapping = {feature: i for i, feature in enumerate(features_anonymize)}
+
+        df = df.rename(columns=mapping)
+
+        for i, transform in enumerate(transforms):
+            # Add a space at the end so we can match the last input
+            transform += " "
+            for key, value in mapping.items():
+                transform = transform.replace(f" {key} ", f" {value} ")
+
+            transforms[i] = transform
+
+    with open(args.output, "w") as f:
+        for transform in transforms:
+            f.write("# " + transform + "\n")
+        df.to_csv(f, index=False, line_terminator="\n")
+
+    # TODO: Generate a mapping so that having the initial data allows you to understand
+    # someone referring to the transformed data
